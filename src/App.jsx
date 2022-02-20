797d6e9446251379d59bbc0bf136f9f8d934e0ab
tsgoswami <trishnangshugoswami@yahoo.in>
2022-02-20 14:21:30 +0530

    fix: state create and props passed to chid

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import RootPage from './pages/RootPage';
+import { lazy, Suspense, useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+const ImageView = lazy(() => import("./components/ImageView/ImageView"));
 
 function App() {
+  const [isHomePage, setIsHomePage] = useState(true);
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [isHomePage, searchQuery]);
+
   return (
-    <RootPage />
+    <>
+      <Navbar
+        setIsHomePage={setIsHomePage}
+        isHomePage={isHomePage}
+        setSearchQuery={setSearchQuery}
+        setPageNumber={setPageNumber}
+      />
+      <Suspense fallback={<div>Loading</div>}>
+        <ImageView
+          isHomePage={isHomePage}
+          searchQuery={searchQuery}
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+        />
+      </Suspense>
+    </>
   );
 }
 
