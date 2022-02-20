5f530be732173bec9dc1eaff269c6b061b312e5d
tsgoswami <trishnangshugoswami@yahoo.in>
2022-02-20 14:17:56 +0530

    fix: Root page removed, feat: Search functionality added in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./Navbar.css";
 
-function Navbar() {
+function Navbar({ setIsHomePage, setSearchQuery, isHomePage, setPageNumber }) {
+  const [search, setSearch] = useState(null);
+  const searchRef = useRef();
+
+  const handleQuery = (e) => {
+    e.preventDefault();
+    if (search !== null && search !== undefined) {
+      setIsHomePage(false);
+      setSearchQuery(search);
+      setPageNumber(1);
+    }
+  };
+
+  const handleState = () => {
+    if (!isHomePage) {
+      setPageNumber(1);
+      setIsHomePage(true);
+      searchRef.current.value = "";
+      setSearch(null);
+    }
+
+    // console.log(searchRef);
+    // console.log("called");
+  };
+
   return (
     <div className="nav-container">
-      <div className="logo-container">Unsplash</div>
+      <div className="logo-container">
+        <span onClick={handleState}>Unsplash</span>
+      </div>
       <div className="search-container">
-        <div className="search-items">
-          <input placeholder="Search" className="search-field" />
-          <button className="filter-button">Filter</button>
-        </div>
+        <form className="search-items" onSubmit={handleQuery}>
+          <input
+            placeholder="Search"
+            className="search-field"
+            onChange={(e) => setSearch(e.target.value)}
+            ref={searchRef}
+          />
+          <button type="submit" className="filter-button" onClick={handleQuery}>
+            Search
+          </button>
+          {!isHomePage && <button className="filter-button">Filter</button>}
+        </form>
       </div>
     </div>
   );
